ff7876394162c0df261fec1815cd4f13dffa41bd
Samuel Rendon <samiretru@gmail.com>
2023-04-26 10:19:15 -0500

    creación de base.html, con barra de navegación. creación de input para código de mesa, creación de aplicación tables

--- ORDERNOWPROJECT/ordernow/settings.py
+++ ORDERNOWPROJECT/ordernow/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'application',
     'accounts',
+    'tables',
 ]
 
 MIDDLEWARE = [
---
+++ ORDERNOWPROJECT/tables/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Table(models.Model):
+    code = models.CharField(max_length=7)
+    capacity = models.IntegerField()
+    
